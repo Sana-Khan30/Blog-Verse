4fc949b5bee0354996603fd311d1f4ab9d1d2ece
Sana khan <sanasamad592@gmail.com>
2026-05-31 07:26:30 -0700

    Reorder and rename user API functions

--- client/src/api/userApi.js
+++ client/src/api/userApi.js
@@ -1,6 +1,6 @@
 import API from './axiosInstance.js';
 
-export const getProfile = (username) => API.get(`/users/${username}`);
-export const updateProfile = (data) => API.put('/users/profile', data);
-export const toggleBookmark = (blogId) => API.put(`/users/bookmark/${blogId}`);
-export const getBookmarks = () => API.get('/users/bookmarks');
+export const getBookmarks    = ()       => API.get('/users/bookmarks');
+export const toggleBookmark  = (blogId) => API.put(`/users/bookmark/${blogId}`);
+export const getUserProfile  = (username) => API.get(`/users/${username}`);
+export const updateProfile   = (data)   => API.put('/users/profile', data);
